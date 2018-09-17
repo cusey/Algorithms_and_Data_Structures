d7c8bff88a6d85b64806e52d58f4b7908886b1b9
John Cusey <jjcusey@gmail.com>
2018-09-17 13:53:25 +0000

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,14 +1,5 @@
 #python 3.5.2
 
-'''
-A stack (sometimes called a “push-down stack”) is an ordered collection of items where the addition of new items and the
-removal of existing items always takes place at the same end. This end is commonly referred to as the “top.” 
-The end opposite the top is known as the “base.”
-
-The base of the stack is significant since items stored in the stack that are closer to the base represent those that have
-been in the stack the longest. The most recently added item is the one that is in position to be removed first. This ordering 
-principle is sometimes called LIFO, last-in first-out.
-'''
 class Stack:
      def __init__(self):
          self.items = []
