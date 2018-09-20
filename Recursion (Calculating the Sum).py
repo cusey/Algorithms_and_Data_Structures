d241a88887e759bafd962a12ac022edea32f9c72
John Cusey <jjcusey@gmail.com>
2018-09-20 11:25:36 +0000

    Update Recursion (Calculating the Sum).py

--- Recursion (Calculating the Sum).py
+++ Recursion (Calculating the Sum).py
@@ -1,26 +1,26 @@
-#python 3.5.2
-
-def listsum(numList):
-   if len(numList) == 1:
-        print(' {} '.format(numList[0]) )
-        print('-'*20)
-        return numList[0]
-   else:
-        print(' {}  + '.format(numList[0]) )
-        return numList[0] + listsum(numList[1:])
-    
-
-
-print(listsum([1,3,5,7,9]))
+'''
 
 '''
-OUTPUT:
+'''
+'''
+'''
+'''
+'''
+'''
+'''
+'''
 
- 1  + 
- 3  + 
- 5  + 
- 7  + 
- 9 
---------------------
-25
+
+'''
+
+'''
+'''
+
+'''
+'''
+'''
+'''
+'''
+'''
+'''
 '''
