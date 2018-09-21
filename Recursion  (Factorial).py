e6d9565bc1f4581a987166bad192b1b7ef170e58
John Cusey <jjcusey@gmail.com>
2018-09-21 07:58:42 -0400

    Update Recursion  (Factorial).py

--- Recursion  (Factorial).py
+++ Recursion  (Factorial).py
@@ -2,29 +2,51 @@
 
 def factorial(n):
     
+    result = 1
+    
+    while n != 1:
+        print( '{}  * {}'.format(result, n) )
+        result = result * n
+        
+        n = n -1
+        
+    return result 
+
+
+def recfactorial(n):
+    
     if n == 1:
         print( ' {} '.format(n ) )
-        print('-'*20)
+        
         return n
     else:
-        print(' {}  * factorial ( {} )'.format(n, n-1) )
-        return n * factorial( n -1 )
+        print(' {}  * recfactorial ( {} )'.format(n, n-1) )
+        return n * recfactorial( n -1 )
     
     
-
     
-print( factorial( 5 ) )
+print( factorial( 5 ) )   
+print('-'*20)
+    
+print( recfactorial( 5 ) )
+print('-'*20)
 
 
 '''
 OUTPUT:
 
- 5  * factorial ( 4 )
- 4  * factorial ( 3 )
- 3  * factorial ( 2 )
- 2  * factorial ( 1 )
- 1 
---------------------
+1  * 5
+5  * 4
+20  * 3
+60  * 2
 120
+--------------------
+ 5  * recfactorial ( 4 )
+ 4  * recfactorial ( 3 )
+ 3  * recfactorial ( 2 )
+ 2  * recfactorial ( 1 )
+ 1 
+120
+--------------------
 
 '''
