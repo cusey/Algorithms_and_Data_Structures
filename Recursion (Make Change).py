9691f5f76bdd37ef48a3647ab8541edfd48ca575
John Cusey <jjcusey@gmail.com>
2018-09-27 15:07:04 -0400

    Update Recursion (Make Change).py

--- Recursion (Make Change).py
+++ Recursion (Make Change).py
@@ -1,5 +1,5 @@
 #python 3.5.2
- 
+
 def makeChange( coinValueList, change, debug ):
     
     count = 0
@@ -32,6 +32,7 @@ def makeChange( coinValueList, change, debug ):
 print ( makeChange( [10,25,5,1] , 63, True ) )
 print('-'*20)
 
+
 class Coins:
 
     def __init__(self, coinValueList):
@@ -55,7 +56,8 @@ class Coins:
                   + ', coinValueList : ' + str( self.coinValueList )
                   + ', coin: '+ str( self.coinValueList[ self.counter ] )
                   + ' ] ' )
-        
+
+    
     def makeCoinChange( self, change ):
         
         print ( 'change: ' + str ( change ) )
@@ -63,26 +65,67 @@ class Coins:
         if change  - self.coin < 0:
              self.counter +=  1
 
-        if change != 0:
-            if self.coin <  change:
+        if change == 0:
+             pass
+        else:
+            if self.coin <= change:
                 self.results[ self.coin ] += 1
                 
             print( self.__repr__() )
 
-            if self.coin <  change:
+            if self.coin <= change:
                 self.makeCoinChange( change - self.coin )
             else:
-                self.counter +=  1
                 self.makeCoinChange( change )
-         else:
-                    print('Yes')
-                
-                
+
         return self.results
+
 
 change = Coins( [10,25,5,1])
 
-
+print('\n')
 print ( change.makeCoinChange( 63 ) )
-
 print('-'*20)
+
+
+'''
+OUTPUT:
+
+COIN VALUES:  [25, 10, 5, 1]
+63 - [ 25 ]
+ = 38
+38 - [ 25 ]
+ = 13
+13 - [ 10 ]
+ = 3
+3 - [ 1 ]
+ = 2
+2 - [ 1 ]
+ = 1
+1 - [ 1 ]
+ = 0
+{25: 2, 10: 1, 5: 0, 1: 3}
+--------------------
+
+
+change: 63
+Coins [ counter : 0, results : {25: 1, 10: 0, 5: 0, 1: 0}, coinValueList : [25, 10, 5, 1], coin: 25 ] 
+change: 38
+Coins [ counter : 0, results : {25: 2, 10: 0, 5: 0, 1: 0}, coinValueList : [25, 10, 5, 1], coin: 25 ] 
+change: 13
+Coins [ counter : 1, results : {25: 2, 10: 1, 5: 0, 1: 0}, coinValueList : [25, 10, 5, 1], coin: 10 ] 
+change: 3
+Coins [ counter : 2, results : {25: 2, 10: 1, 5: 0, 1: 0}, coinValueList : [25, 10, 5, 1], coin: 5 ] 
+change: 3
+Coins [ counter : 3, results : {25: 2, 10: 1, 5: 0, 1: 1}, coinValueList : [25, 10, 5, 1], coin: 1 ] 
+change: 2
+Coins [ counter : 3, results : {25: 2, 10: 1, 5: 0, 1: 2}, coinValueList : [25, 10, 5, 1], coin: 1 ] 
+change: 1
+Coins [ counter : 3, results : {25: 2, 10: 1, 5: 0, 1: 3}, coinValueList : [25, 10, 5, 1], coin: 1 ] 
+change: 0
+{25: 2, 10: 1, 5: 0, 1: 3}
+--------------------
+'''
+
+
+
