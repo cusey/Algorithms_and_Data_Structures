5e5392f0cb80c8a98eefaf3d2847f96c988a9f79
John Cusey <jjcusey@gmail.com>
2018-10-02 09:31:55 -0400

    Update Search - List (Binary).py

--- Search - List (Binary).py
+++ Search - List (Binary).py
@@ -3,8 +3,10 @@
 class search():
     
     def __init__(self, searchList, item):
-        self.searchList = searchList 
+        self.searchList = searchList
+        print( 'List: ', self.searchList )
         self.item = item 
+        print('Number Search For: ', self.item)
         self.front = 0
         self.back = len (searchList) -1 
         self.midpoint = self.mid()
@@ -28,25 +30,29 @@ class search():
                 self.result.append(self.midpoint)
                 
                
-    def sweep(self):
-         
-        #Left Sweep
-        counter = self.midpoint -1
+    def sweep(self,direction):
+        
+        increment = 0
+        
+        if direction == 'RIGHT':
+            increment = 1
+            stopIndex = len(self.searchList) - 1
             
-        while self.item == self.searchList [ counter ] and counter <= 0:
-            self.result.append(counter)
-            counter = counter -1
+        if direction == 'LEFT':
+            increment = - 1
+            stopIndex = 0
+
+        counter = self.midpoint + increment
+        sweepDone = False
+        
+        while counter <= stopIndex and not sweepDone :
+            if self.item == self.searchList [ counter ] :
+                self.result.append(counter)
+            else:
+                sweepDone = True
             
-        #Right Sweep
-        counter = self.midpoint +1
-            
-        while self.item == self.searchList [ counter ] and counter >= len(self.searchList) :
-            self.result.append(counter)
-            counter = counter +1
-            
-            
-    
-                
+            counter = counter + increment
+
     def atEnds(self):
         return self.midpoint == 1 or self.midpoint >= len(self.searchList) -2
                 
@@ -55,27 +61,102 @@ class search():
         while not (self.done or self.atEnds()) :
             self.moveMid()
             
-        self.sweep()
+        self.sweep('RIGHT')
+            
+        self.sweep('LEFT')
             
         return self.result
+    
+def log(line):
+    print ( line )
+    print ( '-'*20 + '\n' )
+    
 
 s0 = search( [5, 10, 15, 20, 25] , 5 ) 
+log( s0.do() ) 
 
-print ( s0.do() ) 
-
-        
 s1 = search( [5, 10, 15, 20, 25] , 10 ) 
-
-print ( s1.do() ) 
+log( s1.do() ) 
 
 s2 = search( [5, 10, 15, 20, 25] , 15 ) 
-
-print ( s2.do() )
+log( s2.do() )
 
 s3 = search( [5, 10, 15, 20, 25] , 20 ) 
-
-print ( s3.do() )
+log( s3.do() )
 
 s4 = search( [5, 10, 15, 20, 25] , 25 ) 
+log( s4.do() )
 
-print ( s4.do() )
+s5 = search( [5, 5, 10, 15, 20, 25] , 5 ) 
+log( s5.do() )
+
+s6 = search( [5, 10, 15, 20, 25, 25] , 25 ) 
+log( s6.do() )
+
+s7 = search( [5, 10, 10, 15, 20, 25, 25] , 10 ) 
+log( s7.do() )
+
+
+'''
+OUPUT:
+
+List:  [5, 10, 15, 20, 25]
+Number Search For:  5
+front : 0  midpoint : 2 back : 4
+front : 0  midpoint : 1 back : 2
+[0]
+--------------------
+
+List:  [5, 10, 15, 20, 25]
+Number Search For:  10
+front : 0  midpoint : 2 back : 4
+front : 0  midpoint : 1 back : 2
+[1]
+--------------------
+
+List:  [5, 10, 15, 20, 25]
+Number Search For:  15
+front : 0  midpoint : 2 back : 4
+[2]
+--------------------
+
+List:  [5, 10, 15, 20, 25]
+Number Search For:  20
+front : 0  midpoint : 2 back : 4
+front : 2  midpoint : 3 back : 4
+[3]
+--------------------
+
+List:  [5, 10, 15, 20, 25]
+Number Search For:  25
+front : 0  midpoint : 2 back : 4
+front : 2  midpoint : 3 back : 4
+[4]
+--------------------
+
+List:  [5, 5, 10, 15, 20, 25]
+Number Search For:  5
+front : 0  midpoint : 2 back : 5
+front : 0  midpoint : 1 back : 2
+[1, 0]
+--------------------
+
+List:  [5, 10, 15, 20, 25, 25]
+Number Search For:  25
+front : 0  midpoint : 2 back : 5
+front : 2  midpoint : 3 back : 5
+front : 3  midpoint : 4 back : 5
+[4, 5]
+--------------------
+
+List:  [5, 10, 10, 15, 20, 25, 25]
+Number Search For:  10
+front : 0  midpoint : 3 back : 6
+front : 0  midpoint : 1 back : 3
+[1, 2]
+--------------------
+
+'''
+
+        
+
